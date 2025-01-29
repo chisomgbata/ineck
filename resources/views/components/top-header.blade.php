2228e8a3eb1d3050946e6aa4797d5908283b9498
Chisom <chisomgbata@gmail.com>
2025-01-29 06:48:23 +0100

    ineck

--- resources/views/components/top-header.blade.php
+++ resources/views/components/top-header.blade.php
@@ -11,14 +11,21 @@
             <div class="w-auto">
                 <div class="flex flex-wrap items-center">
                     <div class="flex items-center">
-                        <a class="px-6 py-3 text-sm font-bold text-yellow-900 hover:underline mr-2"
-                           href="{{ route('votes') }}">VOTES</a>
-                        <a class="px-6 py-3 text-sm font-bold text-yellow-900 hover:underline mr-2"
-                           href="#">LOGIN</a>
-                        <a
-                            class="px-6 py-3 bg-yellow-500 text-sm text-yellow-900 font-bold hover:bg-yellow-400 focus:ring focus:ring-yellow-200 transition duration-200"
-                            href="{{route('register')}}">Get Accredicted
-                        </a>
+                        @guest
+                            <a class="px-6 py-3 text-sm font-bold text-yellow-900 hover:underline mr-2"
+                               href="{{ route('votes') }}">VOTES</a>
+                            <a class="px-6 py-3 text-sm font-bold text-yellow-900 hover:underline mr-2"
+                               href="#">LOGIN</a>
+                            <a
+                                class="px-6 py-3 bg-yellow-500 text-sm text-yellow-900 font-bold hover:bg-yellow-400 focus:ring focus:ring-yellow-200 transition duration-200"
+                                href="{{route('register')}}">Get Accredicted
+                            </a>
+                        @endguest
+                        @auth
+
+                            <livewire:vote/>
+                        @endauth
+
                     </div>
                 </div>
 
---
+++ resources/views/livewire/vote.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <button
+        class="px-6 py-3 bg-yellow-500 text-sm text-yellow-900 font-bold hover:bg-yellow-400 focus:ring focus:ring-yellow-200 transition duration-200"
+    >VOTE NOW
+    </button>
+
+
+</div>
